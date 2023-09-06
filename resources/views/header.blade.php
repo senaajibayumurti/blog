d8ae329245b5d9bb2f1e4b5d59755d224b59314d
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-09-06 21:28:36 +0700

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <header>
+		<h2>Praktikum Pemrograman Web 2 - Tugas Laprak 2</h2>
+		<a href="/home">Home</a> |
+		<a href="/about">About</a>
+        <hr>
+	</header>
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', 'PPW2 - Tugas Laprak/home')
+
+@section('content')
+    @include('header') 
+
+    <h1>Selamat datang di halaman utama!</h1>
+    <p>Ini adalah konten utama halaman.</p>
+@endsection
