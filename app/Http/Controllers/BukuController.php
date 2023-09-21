f063e94045a691f50dc4748346b1f93ea3636c92
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-09-21 12:48:40 +0700

    Minor Changes

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -10,6 +10,12 @@ class BukuController extends Controller
     public function index(){
         $data_buku = Buku::all();
 
-        return view('buku.index', compact('data_buku'));
+        //Var banyak data
+        $jumlah_data = count($data_buku);
+
+        //Var jumlah harga
+        $total_harga = Buku::sum('harga');
+
+        return view('buku.index', compact('data_buku','jumlah_data','total_harga'));
     }
 }
--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -1,23 +1,38 @@
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>id</th>
-            <th>Judul Buku</th>
-            <th>Penulis</th>
-            <th>Harga</th>
-            <th>Tgl. Terbit</th>
-            <th>Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($data_buku as $buku)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>tugas4pertemuan4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <table class="table table-striped">
+        <thead>
             <tr>
-                <td>{{ $buku -> id}}</td>
-                <td>{{ $buku -> judul}}</td>
-                <td>{{ $buku -> penulis}}</td>
-                <td>{{ "RP ".number_format($buku->harga, 2, ',', '.') }}</td>
-                <td>{{ $buku -> tgl_terbit -> format('d/m/y')}}</td>
+                <th>id</th>
+                <th>Judul Buku</th>
+                <th>Penulis</th>
+                <th>Harga</th>
+                <th>Tgl. Terbit</th>
+                <th>Aksi</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($data_buku as $buku)
+                <tr>
+                    <td>{{ $buku -> id}}</td>
+                    <td>{{ $buku -> judul}}</td>
+                    <td>{{ $buku -> penulis}}</td>
+                    <td>{{ "Rp ".number_format($buku->harga, 2, ',', '.') }}</td>
+                    <td>{{ \Carbon\Carbon::parse($buku->tgl_terbit)->format('d/m/y') }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <h3>Banyak Data: {{ $jumlah_data }}</h3>
+    <h3>Jumlah Total Harga: Rp {{ number_format($total_harga, 2, ',', '.') }}</h3>
+</body>
+</html>
